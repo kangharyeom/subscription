2a7c86d035ce1ff980036130a006b11d580e6534
kangharyeom <kangharyeom@gmail.com>
2025-03-07 13:12:29 +0900

    Feature: 공통 모듈 세팅
    - 예외 처리 구현
    - ResponseDto 구현
    - 구독 상태 추가

---
+++ src/main/java/dott/subscription/response/SingleResponseDto.java
@@ -0,0 +1,15 @@
+package dott.subscription.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class SingleResponseDto<T> {
+    private T data;
+
+    @Override
+    public String toString() {
+        return "SingleResponseDto";
+    }
+}

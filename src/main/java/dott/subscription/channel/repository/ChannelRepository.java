1d0b2cfbd80b457c0f84b093ac6a49a0fd47bd99
kangharyeom <kangharyeom@gmail.com>
2025-03-09 01:45:58 +0900

    Refactor: Repository 어노테이션 추가

--- src/main/java/dott/subscription/channel/repository/ChannelRepository.java
+++ src/main/java/dott/subscription/channel/repository/ChannelRepository.java
@@ -2,9 +2,11 @@ package dott.subscription.channel.repository;
 
 import dott.subscription.channel.entity.Channel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface ChannelRepository extends JpaRepository<Channel, Long> {
     Optional<Channel> findByName(String name);
 }
--- src/main/java/dott/subscription/subscriptionHistory/repository/SubscriptionHistoryRepository.java
+++ src/main/java/dott/subscription/subscriptionHistory/repository/SubscriptionHistoryRepository.java
@@ -2,9 +2,11 @@ package dott.subscription.subscriptionHistory.repository;
 
 import dott.subscription.subscriptionHistory.entity.SubscriptionHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface SubscriptionHistoryRepository extends JpaRepository<SubscriptionHistory, Long> {
     List<SubscriptionHistory> findAllByPhoneNumber(String phoneNumber);
 }

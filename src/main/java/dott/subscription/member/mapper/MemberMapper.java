99dd7f7b462f691d1ce5a4868961ea8362732f4f
kangharyeom <kangharyeom@gmail.com>
2025-03-10 00:10:52 +0900

    Refactor: 회원 상세 조회 수정
    - response 에서 null safe 하도록 수정

--- src/main/java/dott/subscription/member/mapper/MemberMapper.java
+++ src/main/java/dott/subscription/member/mapper/MemberMapper.java
@@ -1,8 +1,13 @@
 package dott.subscription.member.mapper;
 
+import dott.subscription.channel.entity.Channel;
+import dott.subscription.constant.SubscriptionStatus;
 import dott.subscription.member.dto.*;
 import dott.subscription.member.entity.Member;
+import dott.subscription.subscription.entity.Subscription;
 import org.mapstruct.Mapper;
+
+import java.util.Optional;
 
 @Mapper(componentModel = "spring")
 public interface MemberMapper {
@@ -12,13 +17,24 @@ public interface MemberMapper {
 
     MemberResponseDto memberToMemberResponseDto(Member member);
     default MemberDetailsResponseDto memberToMemberDetailsResponseDto(Member member){
+        Long channelId = Optional.ofNullable(member.getSubscription())
+                .map(Subscription::getChannel)
+                .map(Channel::getId)
+                .orElse(null);
+        String channelName = Optional.ofNullable(member.getSubscription())
+                .map(Subscription::getChannel)
+                .map(Channel::getName)
+                .orElse(null);
+        SubscriptionStatus subscriptionStatus = Optional.ofNullable(member.getSubscription())
+                .map(Subscription::getSubscriptionStatus)
+                .orElse(null);
 
         return MemberDetailsResponseDto.builder()
                 .id(member.getId())
                 .phoneNumber(member.getPhoneNumber())
-                .channelId(member.getSubscription().getChannel().getId())
-                .channelName(member.getSubscription().getChannel().getName())
-                .subscriptionStatus(member.getSubscription().getSubscriptionStatus())
+                .channelId(channelId)
+                .channelName(channelName)
+                .subscriptionStatus(subscriptionStatus)
                 .createdAt(member.getCreatedAt())
                 .modifiedAt(member.getModifiedAt())
                 .build();
